32b2fea853fd6701450a67103bdeecae417d0315
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-06-20 12:13:43 +0530

    Add project files.

---
+++ Employee1/EmployeeInform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee1
+{
+    public class EmployeeInform
+    {
+        private int id, salary;
+        private string name, city;
+
+        public EmployeeInform(int id, int salary, string name, string city = "pune")
+        {
+            this.id = id;
+            this.salary = salary;
+            this.name = name;
+        }
+        public string Display()
+        {
+            return $"Employee Info{id}\n{salary}\n{name}\n{city}";
+        }
+    }
+}
---
+++ Employee1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeInform inform=new EmployeeInform(101,45000,"Rohan");
+            Console.WriteLine(inform.Display());
+            EmployeeInform inform2 = new EmployeeInform(101, 4500, "Rohan", "Latur");
+            Console.WriteLine(inform2.Display());
+            EmployeeInform inform1= new EmployeeInform(id : 102, salary : 500000, name:"Rakesh",city:"Mumbai") ;
+            Console.WriteLine(inform1.Display());
+        }
+    }
+}
